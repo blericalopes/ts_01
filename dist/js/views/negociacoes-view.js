39b867ed241a6c33aa650f1cc5a999804b90747c
blericalopes <blericalopes@outlook.com>
2023-03-14 10:03:18 -0300

    ts_01

---
+++ dist/js/views/negociacoes-view.js
@@ -0,0 +1,23 @@
+export class NegociacoesView {
+    constructor(seletor) {
+        this.elemento = document.querySelector(seletor);
+    }
+    template() {
+        return `
+            <table class="table table-hover table-bordered">
+                <thead>
+                    <tr>
+                        <th>DATA</th>
+                        <th>QUANTIDADE</th>
+                        <th>VALOR</th>
+                    </tr>
+                </thead>
+            </table>
+            <tbody>
+            </tbody>
+        `;
+    }
+    update() {
+        this.elemento.innerHTML = this.template();
+    }
+}
