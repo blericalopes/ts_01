db9a1cbb0ae7ddbf538d38187dfec476726272e3
blericalopes <blericalopes@outlook.com>
2023-07-01 19:33:34 -0300

    mudança boilerplate

---
+++ app/dist/js/controllers/negociacao-controller.js
@@ -0,0 +1,44 @@
+import { DiasdaSemana } from "../enums/dias-da-semana.js";
+import { Negociacao } from "../models/negociacao.js";
+import { Negociacoes } from '../models/negociacoes.js';
+import { MensagemView } from "../views/mensagem-view.js";
+import { NegociacoesView } from '../views/negociacoes-view.js';
+export class NegociacaoController {
+    constructor() {
+        this.negociacoes = new Negociacoes();
+        this.negociacoesView = new NegociacoesView('#negociacoesView', true);
+        this.mensagemView = new MensagemView('#mensagemView');
+        this.DOMINGO = 0;
+        this.SABADO = 6;
+        this.inputData = document.querySelector("#data");
+        this.inputQuantidade = document.querySelector("#quantidade");
+        this.inputValor = document.querySelector("#valor");
+        this.negociacoesView.update(this.negociacoes);
+    }
+    adiciona() {
+        const negociacao = Negociacao.criaDe(this.inputData.value, this.inputQuantidade.value, this.inputValor.value);
+        if (!this.ehDiaUtil(negociacao.data)) {
+            this.mensagemView.update('Apenas negociações em dias úteis são aceitas');
+            return;
+        }
+        this.negociacoes.adiciona(negociacao);
+        this.limpaFormulario();
+        this.atualizaView();
+    }
+    ehDiaUtil(data) {
+        return data.getDay() >
+            DiasdaSemana.DOMINGO
+            && data.getDay() <
+                DiasdaSemana.SABADO;
+    }
+    limpaFormulario() {
+        this.inputData.value = '';
+        this.inputQuantidade.value = '';
+        this.inputValor.value = '';
+        this.inputData.focus();
+    }
+    atualizaView() {
+        this.negociacoesView.update(this.negociacoes);
+        this.mensagemView.update('Negociação adicionada com sucesso');
+    }
+}
---
+++ app/dist/js/enums/dias-da-semana.js
@@ -0,0 +1,10 @@
+export var DiasdaSemana;
+(function (DiasdaSemana) {
+    DiasdaSemana[DiasdaSemana["DOMINGO"] = 0] = "DOMINGO";
+    DiasdaSemana[DiasdaSemana["SEGUNDA"] = 1] = "SEGUNDA";
+    DiasdaSemana[DiasdaSemana["TERCA"] = 2] = "TERCA";
+    DiasdaSemana[DiasdaSemana["QUARTA"] = 3] = "QUARTA";
+    DiasdaSemana[DiasdaSemana["QUINTA"] = 4] = "QUINTA";
+    DiasdaSemana[DiasdaSemana["SEXTA"] = 5] = "SEXTA";
+    DiasdaSemana[DiasdaSemana["SABADO"] = 6] = "SABADO";
+})(DiasdaSemana || (DiasdaSemana = {}));
