096067a83daf4900b42a99e617093c9db74fb0d9
blericalopes <blericalopes@outlook.com>
2023-08-14 11:49:36 -0300

    implementa metodo logar tempo de execução generico para ser usado em qualquer lugar

--- app/src/decorators/logar-tempo.ts
+++ app/src/decorators/logar-tempo.ts
@@ -4,6 +4,16 @@ export function logarTempo() {
         propertyKey: string, 
         descriptor: PropertyDescriptor
     ) {
+
+        const metodoOriginal = descriptor.value;
+        descriptor.value = function(...args: any []) {
+            const t1 = performance.now();
+            const retorno = metodoOriginal.apply(this, args);
+            const t2 = performance.now();
+            console.log(`${propertyKey}. tempo de execução: ${(t2 - t1)/1000} segundos.`)
+            retorno
+        }
+
         return descriptor;
     }
 }
--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -1,3 +1,5 @@
+import { logarTempo } from "../decorators/logar-tempo.js";
+
 export abstract class View<T> {
 
     protected elemento: HTMLElement;
@@ -15,16 +17,13 @@ export abstract class View<T> {
         }
     }
 
-
+    @logarTempo()
     public update(model: T): void {
-        const t1 = performance.now();
         let template = this.template(model);
         if (this.escapar) {
             template = template.replace(/<script>[\s\S]*?<\/scrip>/, '');
         }
         this.elemento.innerHTML = template;
-        const t2 = performance.now()
-        console.log(`Tempo de execução do método update: ${(t2 - t1)/1000} segundos!`);
     }
 
     protected abstract template(model: T): string;
